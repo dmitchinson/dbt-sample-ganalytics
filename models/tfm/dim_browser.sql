aeb25a87668cae700d85db938be619dfc9cdad47
David Mitchinson <david.mitchinson@newsignature.com>
2021-05-20 15:30:08 +0100

    Incremental dim loads

--- models/tfm/dim_browser.sql
+++ models/tfm/dim_browser.sql
@@ -1,6 +1,17 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+}}
+
 select distinct
     {{ dbt_utils.surrogate_key(['device.browser']) }} as id
     ,device.browser
     ,{{ dbt_utils.current_timestamp() }} as ts_load
 from
     {{ ref('stg_ga_sessions') }}
+
+{% if is_incremental() %}
+    where TIMESTAMP_SECONDS(visitStartTime) >= (select max(ts_load) from {{ this }})
+{% endif %}
--- models/tfm/dim_geo.sql
+++ models/tfm/dim_geo.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+}}
+
 select distinct 
     {{ dbt_utils.surrogate_key([
         'geoNetwork.continent'
@@ -16,3 +23,7 @@ select distinct
     ,{{ dbt_utils.current_timestamp() }} as ts_load
 from
     {{ ref('stg_ga_sessions') }}
+
+{% if is_incremental() %}
+    where TIMESTAMP_SECONDS(visitStartTime) >= (select max(ts_load) from {{ this }})
+{% endif %}
